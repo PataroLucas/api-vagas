3d72728326df26e5e1ee60786378b4870d652921
Lucas Pataro <lucasba.pataro@gmail.com>
2026-07-26 23:18:43 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/PataroLucas/api-vagas/router"
+import (
+	"github.com/PataroLucas/api-vagas/config"
+	"github.com/PataroLucas/api-vagas/router"
+)
+
+var logger *config.Logger
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	error := config.Init()
+	if error != nil {
+		logger.Errorf("config initialization error: %v", error)
+		return
+	}
+
 	router.Initialize()
 }
